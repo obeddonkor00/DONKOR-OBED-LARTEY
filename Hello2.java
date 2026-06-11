ac1389643f5e9c6dd1c54b3e6cf6de4699da2d0f
obeddonkor00 <obeddonkor00@gmail.com>
2026-06-11 06:30:45 +0000

    Add files via upload

---
+++ Hello2.java
@@ -0,0 +1,11 @@
+public class work3 {
+    public static void main(String[] args) {
+        System.out.println("      T      ");
+        System.out.println("     TTT     ");
+        System.out.println("    TTTTTT   ");
+        System.out.println("   TTTTTTT   ");
+        System.out.println("  TTTTTTTTT  ");
+        System.out.println(" TTTTTTTTTTT ");
+        System.out.println("TTTTTTTTTTTTT");
+    }
+}
